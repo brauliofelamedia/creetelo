9b58d1c0cd4f198a36a1da8b60186b17e72ad996
Braulio Miramontes <braulio@webmaker.mx>
2024-11-22 12:15:10 -0700

    Se añade Gravatar

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -21,7 +21,7 @@ class FrontController extends Controller
             $data = $contactServices->getContacts();
         }
 
-        $paginate = $data['meta'];
+        //$paginate = $data['meta'];
 
         return view('front.home', compact('data','name','paginate'));
     }
