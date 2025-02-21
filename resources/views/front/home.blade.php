9fcee2a04a22b3bc4e7243bd5580d8fc35c514a3
Braulio Miramontes <braulio@felamedia.com>
2025-02-21 11:57:43 -0700

    Se hace el recorde de nombres largos en pagina de detalle otros y en home

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -450,8 +450,8 @@
                                         <div class="mt-20 top-grid-1 d-flex flex-column align-items-center justify-content-center">
                                             <div class=" d-flex flex-column align-items-center justify-content-center">
                                                 <h3>
-                                                    @if (strlen($contact->full_name) > 30)
-                                                        {{ Str::limit($contact->full_name, 30).'...' }}
+                                                    @if (strlen($contact->full_name) > 22)
+                                                        {{ Str::limit($contact->full_name, 22).'...' }}
                                                     @else
                                                         {{$contact->full_name }}
                                                     @endif
