b88568d75460f7ef3900db980d6c1c1fd060135d
Braulio Miramontes <braulio@felamedia.com>
2025-02-22 11:57:07 -0700

    Se corrige error con la variable de contact en el detalle del perfil y se quita padding para las card del home

--- resources/views/layouts/parts/navbar.blade.php
+++ resources/views/layouts/parts/navbar.blade.php
@@ -1,11 +1,10 @@
 <div class="theme-main-menu sticky-menu theme-menu-one">
 	<div class="container">
         <div class="row">
-            <div class="col-md-4 col-xs-4 col-5">
+            <div class="col-lg-4 col-xs-4 col-5">
                 <a href="{{route('front.home')}}" class="d-block"><img class="logo" src="{{asset('images/Directorio-Logo.png')}}" alt="{{env('APP_NAME')}}" style="width:230px;"></a>
             </div>
-            <div class="col-md-4 col-xs-5 col-1"></div>
-            <div class="col-md-4 col-xs-3 col-6">
+            <div class="col-lg-3 offset-lg-5 col-xs-3 col-7">
                 <ul class="navbar-menu">
                     @if (auth()->check())
                         <li><a style="font-weight:500;" href="{{route('dashboard.account.index')}}" class="user-account"><i class="fas fa-user"></i> <span class="hidden-phone">Mi Perfil</span></a></li>
